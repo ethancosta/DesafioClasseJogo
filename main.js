5514a8e90c9d79c0fb7c507cceb566092c72937f
ethancosta <ethanmcosta11@gmail.com>
2023-12-17 15:32:14 -0300

    projeto bootcamp

---
+++ main.js
@@ -0,0 +1,33 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+
+        switch(this.tipo) {
+            case 'mago':
+                ataque = 'magia';
+                break;
+            case 'guerreiro':
+                ataque = 'espada';
+                break;
+            case 'monge':
+                ataque = 'artes marciais';
+                break;
+            case 'ninja':
+                ataque = 'shuriken';
+                break;
+            default:
+                ataque = 'ataque desconhecido';
+        }
+
+        console.log(`o ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+let heroi = new Heroi('Spider', 25, 'ninja');
+heroi.atacar();  
